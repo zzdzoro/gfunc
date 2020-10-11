47b723d83230788dff00c855a50a6f0a6756ebc3
zhaozhd <feng799593423@163.com>
2020-10-11 16:14:55 +0800

    no message

--- src/Common.php
+++ src/Common.php
@@ -1,16 +1,15 @@
 <?php
-namespace Gfunc;
+namespace zzdzoro\gfunc;
 /**
  * Description of Common
  *
  * @author zhaozhendong <zhaozhendong1009@163.com>
- * @datetime 2020-10-11  13:27:51
+ * @datetime 2020-10-11  16:11:07
  */
 class Common {
 
     public function index() {
         echo __FILE__;
-        echo __CLASS__;
     }
 
 }
