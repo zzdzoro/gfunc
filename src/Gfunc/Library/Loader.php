76753afcf125ce57bdcc615f9474a1ac6eb6fa3a
zhaozhd <feng799593423@163.com>
2020-10-11 12:51:33 +0800

    one

---
+++ src/Gfunc/Library/Loader.php
@@ -0,0 +1,15 @@
+<?php
+namespace Gfunc\Library;
+/**
+ * Description of Loader
+ *
+ * @author zhaozhendong <zhaozhendong1009@163.com>
+ * @datetime 2020-10-11  12:46:38
+ */
+class Loader {
+
+    public function index() {
+        echo __FILE__;
+    }
+
+}
