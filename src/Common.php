82584ca0863fa25b815d08f3064bd3b8eaa5e8e3
zhaozhd <feng799593423@163.com>
2020-10-11 13:28:45 +0800

    no message

--- src/Common.php
+++ src/Common.php
@@ -1,16 +1,16 @@
 <?php
-namespace Gfunc\Library;
+namespace Gfunc;
 /**
- * Description of Loader
+ * Description of Common
  *
  * @author zhaozhendong <zhaozhendong1009@163.com>
- * @datetime 2020-10-11  12:46:38
+ * @datetime 2020-10-11  13:27:51
  */
-class Loader {
+class Common {
 
     public function index() {
         echo __FILE__;
-        echo __CLASS__;
+        
     }
 
 }
